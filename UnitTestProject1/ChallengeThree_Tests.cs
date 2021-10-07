ca8eb353021633aa02a2ce44d930a437a0ee751d
Taylor <taylor.randol@gmail.com>
2021-10-06 23:47:11 -0400

    Unit Tests and Read me added

--- UnitTestProject1/ChallengeThree_Tests.cs
+++ UnitTestProject1/ChallengeThree_Tests.cs
@@ -8,10 +8,20 @@ namespace UnitTestProject1
     public class ChallengeThree_Tests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SetBadgeID()
         {
             BadgeClass badge = new BadgeClass();
+            badge.BadgeID = 123456;
 
+            double expected = 123456;
+            double actual = badge.BadgeID;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void SetBadgeAndDoorsTest()
+        {
+            BadgeClass badge = new BadgeClass(123456d, new System.Collections.Generic.List<string>() { "A2", "A3" });
         }
     }
 }
--- UnitTestProject1/Challenge_One_Tests.cs
+++ UnitTestProject1/Challenge_One_Tests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Cafe_Console;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace UnitTestProject1
@@ -6,10 +7,50 @@ namespace UnitTestProject1
     [TestClass]
     public class Challenge_One_Tests
     {
+        MenuClass menu = new MenuClass();
         [TestMethod]
-        public void CreateTest()
+        public void SetMealName()
         {
+            MenuClass menu = new MenuClass();
+            menu.MealName = "The BiG FooD Meal";
 
+            string expected = "The BiG FooD Meal";
+            string actual = menu.MealName;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SetMenuNumber()
+        {
+            menu.MealNumber = 15;
+
+            int expected = 15;
+            int actual = menu.MealNumber;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SetDescription()
+        {
+            menu.MealDescription = "Super amazing food! I promise you will love eating it! Best Stuff in the GALAXY";
+
+            string expected = "Super amazing food! I promise you will love eating it! Best Stuff in the GALAXY";
+            string actual = menu.MealDescription;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SetMealPrice()
+        {
+            menu.MealPrice = 9.35m;
+
+            decimal expected = 9.35m;
+            decimal actual = menu.MealPrice;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
